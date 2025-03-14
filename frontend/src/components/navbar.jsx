a9273816820ab73a0a67e86ad60662cdcfb1f058
M-R-0-1 <ramiz01012005@gmail.com>
2025-03-14 23:18:14 +0530

    Error corrected

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "./Navbar.module.css";
+import styles from "./navbar.module.css";
 import { FaInstagram, FaFacebook, FaLinkedin, FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
