d4cb1b5e133f17fc8b0084e104a378c468e89550
M-R-0-1 <ramiz01012005@gmail.com>
2025-03-14 17:41:37 +0530

    Second phase

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,27 +1,75 @@
+/** @format */
+
 import React from "react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import styles from "../pages/home.module.css";
 import axios from "axios";
 import Navbar from "../components/navbar.jsx";
+import Card from "../components/card.jsx";
 
 const Home = () => {
-    const [data, setData] = useState({})
+  const [data, setData] = useState({});
 
-    useEffect(() => {
-        axios.get("http://localhost:5000")
-        .then(res => {
-             setData(res.data);
-             console.log(res.data);
-         })
-    }, [])
+  useEffect(() => {
+    axios.get("http://localhost:5000").then((res) => {
+      setData(res.data);
+      console.log(res.data);
+    });
+  }, []);
 
-    return (
-        <div>
-            <Navbar/>
-            <h1 className={styles.title}>Welcome to Home Page</h1>
-            <p className={styles.events}>{data.Events}</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <div className={`${styles.section} ${styles.centreAlign}`}>
+        <h2 className={styles.heading}>About Us</h2>
+        <p>Welcome to the official web page of Prodigy NIT Trichy.</p>
+      </div>
+
+      <div className={styles.menu}>
+        <div className={styles.menuItem}>Paper Submissions</div>
+        <div className={styles.dot}></div>
+        <div className={styles.menuItem}>Guest Lectures</div>
+        <div className={styles.dot}></div>
+        <div className={styles.menuItem}>Workshops</div>
+        <div className={styles.dot}></div>
+        <div className={styles.menuItem}>Events</div>
+      </div>
+
+      <div className={`${styles.section} ${styles.leftAlign}`}>
+        <h2 className={styles.heading}>Events</h2>
+        <Card title='Events' image='images/' />
+      </div>
+
+      <div className={`${styles.section} ${styles.rightAlign}`}>
+        <h2 className={styles.heading}>Workshops</h2>
+      </div>
+
+      <div className={`${styles.section} ${styles.centreAlign}`}>
+        <h2 className={styles.heading}>Guest Lectures</h2>
+        <p>Space for sth if needed</p>
+      </div>
+
+      <div className={`${styles.section} ${styles.centreAlign}`}>
+        <h2 className={styles.heading}>Meet our Team</h2>
+        <p>Space for sth if needed</p>
+      </div>
+
+      <div className={`${styles.section} ${styles.leftAlign}`}>
+        <h3 className={styles.heading}>Excomms</h3>
+        <p>Space for sth if needed</p>
+      </div>
+
+      <div className={`${styles.section} ${styles.rightAlign}`}>
+        <h3 className={styles.heading}>Core</h3>
+        <p>Space for sth if needed</p>
+      </div>
+
+      <div className={`${styles.section} ${styles.leftAlign}`}>
+        <h4 className={styles.heading}>We are ready to consult you</h4>
+        <p>Space for sth if needed</p>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
